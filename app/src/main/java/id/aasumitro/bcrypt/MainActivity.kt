8decf56967ad41b117a6133aaf3443bd6d4e1681
A. A. Sumitro <hello@aasumitro.id>
2019-10-23 12:56:29 +0800

    Initial commit

---
+++ app/src/main/java/id/aasumitro/bcrypt/MainActivity.kt
@@ -0,0 +1,25 @@
+package id.aasumitro.bcrypt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val hash = BCryptPasswordEncoder(
+            BCryptPasswordEncoder.BCryptVersion.`$2Y`
+        ).encode("secret")
+
+        Log.d("Password Hash", hash)
+
+        if (BCrypt.checkpw("secret", hash)) {
+            Log.d("Password Hash", "It matches")
+        } else {
+            Log.d("Password Hash", "It does not match")
+        }
+    }
+}
